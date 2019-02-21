a7f64d63f4da4428e177b05ec58f40091f019462
Shelby <shelbyyoung003@gmail.com>
2019-02-21 11:15:10 -0700

    Added People js

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 import { people } from './people.js'
 
+let nameElement = document.querySelector(".name")
+let genderElement = document.querySelector(".gender")
+
+nameElement.textContent = people[4].name
+genderElement.textContent = people[4].gender
+
 console.log(people);
