ab88fc5a637b4864a2760cd192a323bbed50c00b
Unknown <shelbyyoung003@gmail.com>
2019-02-26 10:51:30 -0700

    Set up Basic Star Wars Names

--- main.js
+++ main.js
@@ -7,3 +7,20 @@ nameElement.textContent = people[4].name
 genderElement.textContent = people[4].gender
 
 console.log(people);
+
+const intro = document.querySelector(".intro")
+
+const sorted = name.sort((a, b) => (a.name_id > b.name_id) ?  1: -1
+
+.people.forEach(people =>{
+let tile = document.createElement('div')
+let nameElement = document.createElement('h1')
+intro.appendChild(nameElement)
+nameElement.textContent = people.name
+
+let genderElement = document.createElement('h2')
+intro.appendChild(genderElement)
+genderElement.textContent = people.gender
+
+intro.appendChild(tile)
+}))
