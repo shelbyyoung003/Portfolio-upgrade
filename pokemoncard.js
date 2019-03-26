476e6b482156130db635307f958f28baf556136a
Shelby <shelbyyoung003@gmail.com>
2019-03-26 11:03:36 -0600

    Fetch and Functions

--- pokemoncard.js
+++ pokemoncard.js
@@ -1,3 +1,23 @@
 import { pokemon } from "./pokemon.js"
 
-console.log(pokemon)
+fetch('https://pokeapi.co/api/v2/pokemon/25/')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    console.log(myJson);
+    createPokeCard(myJson);
+  });
+
+  const mainContainer = document.querySelector('.container')
+
+  function createPokeCard(pokedata) {
+      let card = document.createElement('div')
+      let name = document.createElement('p')
+
+      name.textContent = pokedata.name
+      card.appendChild(name)
+      mainContainer.appendChild(card)
+  }
+
+//console.log(pokemon)
