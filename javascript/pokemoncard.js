06b3403bbfcbe2fb2467d9944da969bba122b403
Shelby <shelbyyoung003@gmail.com>
2019-03-28 10:43:08 -0600

    Addded Images and Constructor

--- javascript/pokemoncard.js
+++ javascript/pokemoncard.js
@@ -1,4 +1,13 @@
-import { pokemon } from "../Data/pokemon.js"
+import { pokemon } from "../data/pokemon.js"
+
+class Pokemon {
+  constructor(id, name) {
+    this.id = id,
+    this.name = name
+  }
+}
+
+let newPokemon = new Pokemon(80, "PowerfulMon")
 
 pokemon.forEach((singleMon) => {
     fetch(singleMon.url)
@@ -6,22 +15,27 @@ pokemon.forEach((singleMon) => {
     return response.json();
   })
   .then(function(myJson) {
-    console.log(myJson);
+    //console.log(myJson);
     createPokeCard(myJson);
   });
 })
 
   const mainContainer = document.querySelector('.container')
 
-  function createPokeCard(pokedata) {
+  function createPokeCard(pokeData) {
       let card = document.createElement('div')
-      let name = document.createElement('p')
+      card.className = 'box'
+      let figure = document.createElement('figure')
+      let caption = document.createElement('figcaption')
       let image = document.createElement('img')
 
-      name.textContent = pokedata.name
-      image.src =pokedata.sprites.front_default
-      card.appendChild(name)
-      card.appendChild(image)
+
+      let upperName = pokeData.name.charAt(0).toUpperCase() + pokeData.name.slice(1)
+      caption.textContent = upperName
+      image.src = `../media/${pokeData.id}${upperName}.png`
+      figure.appendChild(image)
+      figure.appendChild(caption)
+      card.appendChild(figure)
       mainContainer.appendChild(card)
   }
 
