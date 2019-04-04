afff40004cc02b91649cdd6292297cb849e4ca73
Shelby <shelbyyoung003@gmail.com>
2019-04-04 10:52:57 -0600

    Removed Bulma - Started to Rework

--- javascript/pokemoncard.js
+++ javascript/pokemoncard.js
@@ -1,26 +1,14 @@
 import { pokemon } from '../data/pokemon.js'
 
-class Pokemon {
-  constructor(id) {
-    this.id = id
-  }
-}
-
 const mainContainer = document.querySelector('.container')
 
-function createPokeCard(pokeData) {
-  let infoContainer = document.createElement('div')
-  infoContainer.className = "card_face card_face--back"
-
-  let card = document.createElement('div')
-  card.className = 'box card'
+function cardFront(pokeData) {
+  let cardFront = document.createElement('div')
+  cardFront.className = 'card_face'
   let figure = document.createElement('figure')
-  figure.className = 'card_face'
   let caption = document.createElement('figcaption')
   let image = document.createElement('img')
 
-
-  //let upperName = pokeData.name.charAt(0).toUpperCase() + pokeData.name.slice(1)
   caption.textContent = pokeData.name
   if (pokeData.id !== 0) {
     //image.src = `../images/${pokeData.imageID}${pokeData.name}.png`
@@ -29,15 +17,57 @@ function createPokeCard(pokeData) {
     image.src = `../images/pokeball.png`
   }
 
+  figure.appendChild(image)
+  figure.appendChild(caption)
+  cardFront.appendChild(figure)
+  return cardFront
+}
+
+function cardBackInfo(pokeData) {
+  let infoDiv = document.createElement('div')
+  let moveOne = document.createElement('p')
+  let moveTwo = document.createElement('p')
+  let moveThree = document.createElement('p')
+  let moveFour = document.createElement('p')
+
+  moveOne.textContent = pokeData.moves[0].move.name
+  moveTwo.textContent = pokeData.moves[1].move.name
+  moveThree.textContent = pokeData.moves[2].move.name
+  moveFour.textContent = pokeData.moves[3].move.name
+  
+  infoDiv.appendChild(moveOne)
+  infoDiv.appendChild(moveTwo)
+  infoDiv.appendChild(moveThree)
+  infoDiv.appendChild(moveFour)
+
+  return infoDiv
+}
+
+function cardBack(pokeData) {
+  let cardBack = document.createElement('div')
+  let backImage = document.createElement('img')
+  backImage.className = 'backImage'
+  backImage.src = '../images/pokeball.png'
+  cardBack.classname = 'card_face card_face--back'
+
+  cardBack.appendChild(backImage)
+  cardBack.appendChild(cardBackInfo(pokeData))
+  
+  return cardBack
+}
+
+function createPokeCard(pokeData) {
+  let scene = document.createElement('div')
+  scene.className = 'scene'
+  let card = document.createElement('div')
+  card.className = 'card'
+
+  card.appendChild(cardFront(pokeData))
+  card.appendChild(cardBack(pokeData))
+
   card.addEventListener('click', function () {
     card.classList.toggle("is-flipped")
   })
-
-  figure.appendChild(image)
-  figure.appendChild(caption)
-  card.appendChild(figure)
-  card.appendChild(infoContainer)
-  mainContainer.appendChild(card)
 }
 
 pokemon.forEach(singleMon => {
@@ -53,6 +83,9 @@ pokemon.forEach(singleMon => {
 
 
 function matchIdToImage(aPokemon) {
+  if(aPokemon.id === 0) {
+    aPokemon.imageID = 0
+  }
   if(aPokemon.id < 10) {
       aPokemon.imageID = "00" + aPokemon.id
   }
@@ -66,6 +99,8 @@ function matchIdToImage(aPokemon) {
   return aPokemon
 }
 
+
+
 function fetchSinglePokemon(id) {
   fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
   .then(function(response) {
@@ -76,12 +111,19 @@ function fetchSinglePokemon(id) {
   })
 }
 
+class Pokemon {
+  constructor(name) {
+    this.id = 0
+    this.name = name
+  }
+}
 
 const newPokemonButton = document.querySelector('button')
 
 newPokemonButton.addEventListener('click', function() {
-  let pokemonID = prompt('Enter an ID of an existing pokemon:')
-  fetchSinglePokemon(pokemonID)
+  createPokeCard(matchIdToImage(new Pokemomn('MysteryMon')))
+  // let pokemonID = prompt('Enter an ID of an existing pokemon:')
+  // fetchSinglePokemon(pokemonID)
 });
 
 //console.log(pokemon)
