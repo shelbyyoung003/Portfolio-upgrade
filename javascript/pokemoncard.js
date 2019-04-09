ff1450e04ad0bf38d5ac7283b6e40d3bae0b2e65
Shelby <shelbyyoung003@gmail.com>
2019-04-09 10:23:36 -0600

    Create Pokemon + Header

--- javascript/pokemoncard.js
+++ javascript/pokemoncard.js
@@ -10,13 +10,13 @@ function cardFront(pokeData) {
     let image = document.createElement('img')
 
     caption.textContent = pokeData.name
-    if(pokeData.id !== 0) {
+    if (pokeData.id !== 0) {
         //image.src = `../images/${pokeData.imageID}${pokeData.name}.png`
         image.src = pokeData.sprites.front_default
     } else {
         image.src = `../images/pokeball.png`
     }
-    
+
     figure.appendChild(image)
     figure.appendChild(caption)
     cardFront.appendChild(figure)
@@ -61,9 +61,9 @@ function createPokeCard(pokeData) {
     card.appendChild(cardFront(pokeData))
     card.appendChild(cardBack(pokeData))
 
-    card.addEventListener( 'click', function() {
+    card.addEventListener('click', function () {
         card.classList.toggle('is-flipped');
-      })
+    })
 
     scene.appendChild(card)
     mainContainer.appendChild(scene)
@@ -71,25 +71,25 @@ function createPokeCard(pokeData) {
 
 pokemon.forEach(singleMon => {
     fetch(singleMon.url)
-    .then(function(response) {
-      return response.json()
-    })
-    .then(function(myJson) {
-      createPokeCard(matchIdToImage(myJson))
-    })
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (myJson) {
+            createPokeCard(matchIdToImage(myJson))
+        })
 })
 
 function matchIdToImage(aPokemon) {
-    if(aPokemon.id === 0) {
+    if (aPokemon.id === 0) {
         aPokemon.imageID = 0
     }
-    if(aPokemon.id < 10) {
+    if (aPokemon.id < 10) {
         aPokemon.imageID = "00" + aPokemon.id
     }
-    if(aPokemon.id > 9 && aPokemon.id < 100 ) {
+    if (aPokemon.id > 9 && aPokemon.id < 100) {
         aPokemon.imageID = "0" + aPokemon.id
     }
-    if(aPokemon.id > 99) {
+    if (aPokemon.id > 99) {
         aPokemon.imageID = aPokemon.id
     }
     aPokemon.name = aPokemon.name.charAt(0).toUpperCase() + aPokemon.name.slice(1)
@@ -98,27 +98,56 @@ function matchIdToImage(aPokemon) {
 
 function fetchSinglePokemon(id) {
     fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
-    .then(function(response) {
-        return response.json()
-    })
-    .then(function(retrievedPokemon) {
-        createPokeCard(matchIdToImage(retrievedPokemon))
-    })
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (retrievedPokemon) {
+            createPokeCard(matchIdToImage(retrievedPokemon))
+        })
 }
 
 class Pokemon {
     constructor(name) {
         this.id = 0,
-        this.name = name
+            this.name = name
+        this.moves = [
+            {
+                move: {
+                    name: "Stealth"
+                }
+            },
+            {
+                move: {
+                    name: "Illusion"
+                }
+            },
+            {
+                move: {
+                    name: "Sneak Slash"
+                }
+            },
+            {
+                move: {
+                    name: "Disappear"
+                }
+
+            }
+        ]
     }
 }
 
 const newPokemonButton = document.querySelector('button')
 
-newPokemonButton.addEventListener('click', function() {
-    //createPokeCard(matchIdToImage(new Pokemon('MysteryMon')))
-    let pokemonID = prompt('Enter an ID of an existing pokemon:')
-    fetchSinglePokemon(pokemonID)
-  });
+newPokemonButton.addEventListener('click', function () {
+    createPokeCard(matchIdToImage(new Pokemon('MysteryMon')))
+    // let pokemonID = prompt('Enter an ID of an existing pokemon:')
+    // fetchSinglePokemon(pokemonID)
+});
 
-//console.log(pokemon)
+ // const newPokemonButton = document.querySelector('button')
+
+// newPokemonButton.addEventListener('click', function() {
+//     createPokeCard(matchIdToImage(new Pokemon('MysteryMon')))
+//     // let pokemonID = prompt('Enter an ID of an existing pokemon:')
+//     // fetchSinglePokemon(pokemonID)
+//   });
